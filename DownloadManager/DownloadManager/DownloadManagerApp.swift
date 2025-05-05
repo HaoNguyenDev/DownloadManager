4e79639e4af8063b65d6289eed959a6faf890d6d
HaoNguyen <>
2025-05-05 19:57:35 +0700

    create Download Manager project

---
+++ DownloadManager/DownloadManager/DownloadManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadManagerApp.swift
+//  DownloadManager
+//
+//  Created by Hao Nguyen on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct DownloadManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
